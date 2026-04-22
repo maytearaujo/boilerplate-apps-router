caf553df46e36c05e862e2057ce779fd52259746
Maytê <maytearaujo@yahoo.com.br>
2026-04-22 11:28:25 -0300

    test: verify lint-staged works in pre-commit

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react'
 
-const Main = () =>{ 
-return (
+const Main = () => {
+  return (
     <div>
-        <h1>React Avançado</h1>
+      <h1>React Avançado</h1>
     </div>
-    )
+  )
 }
 
-export default Main;
+export default Main
+// test comment
